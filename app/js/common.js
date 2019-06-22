a36ddd40209d2bfd81bb93f6b6033bbcfd9cb48b
Maxim <minytian@yandex.ru>
2019-06-22 03:37:18 +0300

    Создал меню. Сделал анимацию. Подключил мменю и настроил его в common.js

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,34 @@
 $(function() {
 
-	// Custom JS
+	$('#my-menu').mmenu({
+		extensions: ['widescreen', 
+								'theme-black',  // Темная тема
+								'effect-menu-slide', // Эфект слайда
+								'pagedim-black',	// Затемнение фона при открытие меню
+								'position-right',	//Открывается с права
+								'border-none'			// Убрал линии
+								],
 
+		navbar: {
+			title: '<img src="img/logo-1.svg" alt="салон красоты">'
+		},
+
+		classNames: {
+			vertical: 'expand', // Добавить класс на подменю .Vertical
+												// expand делает выезд подменю вертикально
+
+		},
+	});
+
+	// Объявляем переменную для гамбургера 
+	let smenaHamburgera = $('#my-menu').data('mmenu');
+
+	// Когда гамбургер открыт, сменяем иконку на крестик
+	smenaHamburgera.bind('openPanel:start', function() {
+		$('.hamburger').addClass('is-active');
+	}).bind('openPanel:finish', function() {
+		$('.hamburger').removeClass('is-active');
+	});
+
+	
 });
--- gulpfile.js
+++ gulpfile.js
@@ -46,6 +46,7 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/js/common.js', // Always at the end
+		'app/libs/mmenu/dist/mmenu.js'		//Подключаем mmenu
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
